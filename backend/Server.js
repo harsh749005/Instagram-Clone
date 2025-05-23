a88343c834e3a95e7012d948589f31f008e96725
Harsh <patelharsh749005@gmail.com>
2025-05-23 14:08:13 +0530

    sevre handles the data received

--- backend/Server.js
+++ backend/Server.js
@@ -4,18 +4,27 @@ const PORT = 3000;
 const cors = require("cors");
 
 
+const userModel = require("./models/usermodel");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 
 
 app.get("/",(req,res)=>{
     res.send("Hey this is server");
 })
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async (req,res)=>{
     const {username,email,password,fullname}= req.body;
-    console.log(username);
+    let user = await userModel.create({
+        username,
+        fullname,
+        password,
+        email
+    });
+
+    res.status(200).json({message:"Success",user});
 })
 
 
