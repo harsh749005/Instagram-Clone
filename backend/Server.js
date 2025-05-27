3a5193b12623906395abe1a0cc4a037a161bbb45
Harsh <patelharsh749005@gmail.com>
2025-05-27 11:10:59 +0530

    login and register done in server

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const {generateToken} = require("./utils/generateToken")
 
 const userModel = require("./models/usermodel");
 app.use(cors());
@@ -28,13 +29,27 @@ app.post("/signup",async (req,res)=>{
                 password : hash,
                 email
             });
-            res.status(200).json({message:"Success",user,status:"user created"});
+            const Token = generateToken(user);
+            res.cookie("Token",Token);
+            res.status(200).json({message:"user created",user,status:"success"});
             
         })
     })
 
 })
 
+app.post("/login",async (req,res)=>{
+    const {email,password} = req.body;
+    let user = await userModel.findOne({email:email});
+    if(!user) return res.json({message:"Incorrect Email or Password",status:"401"});
+    bcrypt.compare(password,user.password, function(err,result){
+        if(!result) return res.status(401).send("Incorrect Password");
+        const Token = generateToken(user);
+        res.cookie("Token",Token);
+        console.log(Token);
+        res.status(200).json({message:"login success",user,status:"success"});
+    })
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
